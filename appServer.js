ee06be9e641a25414b7ecac7f9e3762402f8c14b
pol <paulsteven.nadera@gmail.com>
2019-04-18 03:07:58 +0800

    Connected back-end to DB

--- appServer.js
+++ appServer.js
@@ -2,12 +2,24 @@
 const http = require('http')
 
 const express = require('express');
+
+const db = require('./app/database/database');
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+//test connection
+db.execute('SELECT * FROM Item')
+.then(res => {
+	console.log(res);
+})
+.catch(err => {
+	console.log(err);
+});
 
 app.listen(port);
 
 console.log(`API Server started on port ${port}`);
 
-var routes = require('./app/routes/appRoutes'); 
-routes(app); 
+// var routes = require('./app/routes/appRoutes'); 
+// routes(app); 
