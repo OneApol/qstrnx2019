ec719491d7e25ed971f731152bfe6d0b9c36c0fb
pol <paulsteven.nadera@gmail.com>
2019-04-18 02:09:45 +0800

    Create DB and Populate Item Table
    Run tablemaker and then datamaker to populate

---
+++ DB-queries/datamaker.sql
@@ -0,0 +1,9 @@
+USE inventory_qstrnx;
+
+INSERT INTO Item (item_name, item_qty, item_amount) 
+VALUES
+	('Stick-O', 20, 1.50),
+	('Hany', 50, 1.00),
+	('Chocko-Chocko', 100, 2.50),
+	('Potchi', 300, 1.00),
+	('Yacky', 150, 2.00);
